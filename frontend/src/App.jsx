cd1845196effe5d17e7a9f064aa66a7032762574
CalvinChan <calvin@nanoinsure.com>
2023-12-17 21:08:42 +0800

    FEAT: Create react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,14 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
-import SideBar from './components/SideBar';
+import Home from './pages/Home';
 
 function App() {
-  const chats = [
-    { sessionId: 1, name: 'Business Strategies Discussion' },
-    { sessionId: 2, name: 'Marketing Innovations' },
-    { sessionId: 3, name: 'Tech Trends 2022' },
-    { sessionId: 4, name: 'Startups Ideas Exchange' },
-    { sessionId: 5, name: 'AI and Machine Learning' },
-    { sessionId: 6, name: 'Web Development Best Practices' },
-    { sessionId: 7, name: 'Data Science Insights' },
-    { sessionId: 8, name: 'Product Design Workshop' },
-    { sessionId: 9, name: 'Entrepreneurship Stories' },
-    { sessionId: 10, name: 'Investment Opportunities' },
-  ];
-
   return (
-    <div>
-      <SideBar chats={chats} />
-    </div>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
