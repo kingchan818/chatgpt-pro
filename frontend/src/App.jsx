e73cc196e34b603c60ce6a6c5cab0f18c78f7d30
CalvinChan <calvin@nanoinsure.com>
2023-12-17 15:21:53 +0800

    FEAT(GPTCLONE-19): Create sidebar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css';
 import React from 'react';
+import SideBar from './components/SideBar';
 
 function App() {
-  return (
-    <>
+  const chats = [
+    { sessionId: 1, name: 'Business Strategies Discussion' },
+    { sessionId: 2, name: 'Marketing Innovations' },
+    { sessionId: 3, name: 'Tech Trends 2022' },
+    { sessionId: 4, name: 'Startups Ideas Exchange' },
+    { sessionId: 5, name: 'AI and Machine Learning' },
+    { sessionId: 6, name: 'Web Development Best Practices' },
+    { sessionId: 7, name: 'Data Science Insights' },
+    { sessionId: 8, name: 'Product Design Workshop' },
+    { sessionId: 9, name: 'Entrepreneurship Stories' },
+    { sessionId: 10, name: 'Investment Opportunities' },
+  ];
 
-    </>
+  return (
+    <div>
+      <SideBar chats={chats} />
+    </div>
   );
 }
 
