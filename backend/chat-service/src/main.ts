7e1d1b3ef1207ca14f1f4069fa15c287473daa26
CalvinChan <calvin@nanoinsure.com>
2023-12-25 19:20:41 +0800

    FEAT: Add requestIntercepter

--- backend/chat-service/src/main.ts
+++ backend/chat-service/src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { CustomLoggerService } from './logger/logger.service';
+import { RequestInterceptor } from './request/request.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: false });
   app.useLogger(app.get(CustomLoggerService));
+  app.useGlobalInterceptors(new RequestInterceptor());
   const configuration = app.get(ConfigService);
-
   await app.listen(configuration.get<number>('http.port'));
 }
 bootstrap();
--- backend/ocr-service/src/main.ts
+++ backend/ocr-service/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { CustomLoggerService } from './logger/logger.service';
+import { RequestInterceptor } from './request/request.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: false });
   app.useLogger(app.get(CustomLoggerService));
+  app.useGlobalInterceptors(new RequestInterceptor());
   const configuration = app.get(ConfigService);
   await app.listen(configuration.get<number>('http.port'));
 }
