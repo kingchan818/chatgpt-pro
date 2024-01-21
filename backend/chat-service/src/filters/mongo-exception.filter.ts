c08b0cde0a0bc8a9e63231f10b997f7180e38f10
CalvinChan <calvin@nanoinsure.com>
2024-01-21 19:17:53 +0800

    FIX: Hide sensitive information when throwing error in response

--- backend/chat-service/src/filters/mongo-exception.filter.ts
+++ backend/chat-service/src/filters/mongo-exception.filter.ts
@@ -1,6 +1,7 @@
 import { Catch, ExceptionFilter, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
 import { MongoError } from 'mongodb';
 import { Request, Response } from 'express';
+import { set } from 'lodash';
 
 @Catch(MongoError)
 export class MongoExceptionFilter extends HttpException implements ExceptionFilter {
@@ -8,12 +9,20 @@ export class MongoExceptionFilter extends HttpException implements ExceptionFilt
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-
-    response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-      mongodbErrorCode: exception.code,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-      message: exception.message,
-    });
+    const responseObj = {};
+    switch (exception.code) {
+      case 11000:
+        set(responseObj, 'mongodbErrorCode', exception.code);
+        set(responseObj, 'timestamp', new Date().toISOString());
+        set(responseObj, 'path', request.url);
+        set(responseObj, 'message', 'Duplicate key error');
+        break;
+      default:
+        set(responseObj, 'mongodbErrorCode', exception.code);
+        set(responseObj, 'timestamp', new Date().toISOString());
+        set(responseObj, 'path', request.url);
+        break;
+    }
+    response.status(HttpStatus.INTERNAL_SERVER_ERROR).json(responseObj);
   }
 }
