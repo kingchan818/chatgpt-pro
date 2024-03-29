8ed731f579f5c5d2244f89d1dc3e7a2ff2593a21
CalvinChan <calvin@nanoinsure.com>
2024-03-30 00:15:07 +0800

    FIX: Allow to dynamic change the backend endpoint via env

--- frontend/src/domain/endpoints.js
+++ frontend/src/domain/endpoints.js
@@ -1,4 +1,7 @@
-const BASE_URL = 'http://localhost:8181';
+const protocol = process.env.BACKEND_PROTOCOL || 'http';
+const host = process.env.BACKEND_HOST || 'localhost';
+const port = process.env.BACKEND_PORT || '8181';
+const BASE_URL = `${protocol}://${host}:${port}`;
 
 const CREATE_CHAT_ENDPOINT = `${BASE_URL}/chat/create`;
 const CONTINUE_CHAT_ENDPOINT = `${BASE_URL}/chat/continue`;
