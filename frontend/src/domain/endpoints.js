b90cb897428927b3d5d81d1c4891921fcc19ccdf
CalvinChan <calvin@nanoinsure.com>
2024-03-30 00:50:22 +0800

    FIX: Env variable in frontend need to have react prefix

--- frontend/src/domain/endpoints.js
+++ frontend/src/domain/endpoints.js
@@ -1,6 +1,6 @@
-const protocol = process.env.BACKEND_PROTOCOL || 'http';
-const host = process.env.BACKEND_HOST || 'localhost';
-const port = process.env.BACKEND_PORT || '8181';
+const protocol = process.env.REACT_APP_BACKEND_PROTOCOL || 'http';
+const host = process.env.REACT_APP_BACKEND_HOST || 'localhost';
+const port = process.env.REACT_APP_BACKEND_PORT || '8181';
 const BASE_URL = `${protocol}://${host}:${port}`;
 
 const CREATE_CHAT_ENDPOINT = `${BASE_URL}/chat/create`;
