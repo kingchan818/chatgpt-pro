5d301e9e06adb4c9ac61a5fc97cdcb1f51a92281
CalvinChan <calvin@nanoinsure.com>
2024-03-30 21:22:34 +0800

    CI: Allow to inject env variable in frontend runtime

--- frontend/src/domain/endpoints.js
+++ frontend/src/domain/endpoints.js
@@ -1,6 +1,8 @@
-const protocol = process.env.REACT_APP_BACKEND_PROTOCOL || 'http';
-const host = process.env.REACT_APP_BACKEND_HOST || 'localhost';
-const port = process.env.REACT_APP_BACKEND_PORT || '8181';
+import { env } from '../env'
+
+const protocol = env.REACT_APP_BACKEND_PROTOCOL || 'http';
+const host = env.REACT_APP_BACKEND_HOST || 'localhost';
+const port = env.REACT_APP_BACKEND_PORT || '8181';
 const BASE_URL = `${protocol}://${host}:${port}`;
 
 const CREATE_CHAT_ENDPOINT = `${BASE_URL}/chat/create`;
