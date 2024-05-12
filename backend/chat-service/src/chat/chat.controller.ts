bb2e2c2a78856d6749b1ad5a0f4aaa6f94c318c2
kingchan818 <chancch70@gmail.com>
2024-05-12 14:46:04 +0800

    FIX: Transaction error

--- backend/chat-service/src/chat/chat.controller.ts
+++ backend/chat-service/src/chat/chat.controller.ts
@@ -9,7 +9,6 @@ import { ChatService } from './chat.service';
 import { ChatDto } from './dto/chat.dto';
 import { HttpStatusCode } from 'axios';
 import { ChatGuard } from './chat.guard';
-import { sessionTransaction } from 'src/utils/common';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 import { ApiKeyService } from 'src/api-key/api-key.service';
@@ -37,40 +36,31 @@ export class ChatController {
       model: gptModelType,
     });
 
-    const result = await sessionTransaction(this.connection, async (session) => {
-      // create system message and user message
-      const collectionId = generateUniqueKey();
-      const sessionResult = await this.transactionService.create(
-        requestId,
-        [
-          {
-            messageId: generateUniqueKey(),
-            collectionId,
-            role: 'system',
-            apiTokenRef: currentUser.userApiKey,
-            message: body.chatOptions?.systemMessage || this.configService.get('chatgpt.systemMessage'),
-            chatOptions: body.chatOptions,
-            tokenUsage: { usedTokens, usedUSD },
-            llmType: gptModelType,
-          },
-          {
-            messageId: generateUniqueKey(),
-            collectionId,
-            role: 'user',
-            apiTokenRef: currentUser.userApiKey,
-            message: body.message,
-            chatOptions: body.chatOptions,
-            tokenUsage: { usedTokens, usedUSD },
-            llmType: gptModelType,
-          },
-        ],
-        session,
-      );
-      await this.apiKeyService.updateUsageCount(currentUser.userApiKey, { usageCount: usedUSD }, session);
-      return sessionResult;
-    });
-
-    return result;
+    const collectionId = generateUniqueKey();
+    const sessionResult = await this.transactionService.create(requestId, [
+      {
+        messageId: generateUniqueKey(),
+        collectionId,
+        role: 'system',
+        apiTokenRef: currentUser.userApiKey,
+        message: body.chatOptions?.systemMessage || this.configService.get('chatgpt.systemMessage'),
+        chatOptions: body.chatOptions,
+        tokenUsage: { usedTokens, usedUSD },
+        llmType: gptModelType,
+      },
+      {
+        messageId: generateUniqueKey(),
+        collectionId,
+        role: 'user',
+        apiTokenRef: currentUser.userApiKey,
+        message: body.message,
+        chatOptions: body.chatOptions,
+        tokenUsage: { usedTokens, usedUSD },
+        llmType: gptModelType,
+      },
+    ]);
+    await this.apiKeyService.updateUsageCount(currentUser.userApiKey, { usageCount: usedUSD });
+    return sessionResult;
   }
 
   @Sse('sse/:collectionId')
@@ -114,25 +104,18 @@ export class ChatController {
       model: gptModelType,
     });
 
-    const result = await sessionTransaction(this.connection, async (session) => {
-      const transactionResult = await this.transactionService.create(
-        requestId,
-        {
-          messageId: generateUniqueKey(),
-          collectionId: body.collectionId,
-          apiTokenRef: currentUser.userApiKey,
-          role: 'user',
-          message: body.message,
-          tokenUsage: { usedTokens, usedUSD },
-          llmType: gptModelType,
-        },
-        session,
-      );
-
-      await this.apiKeyService.updateUsageCount(currentUser.userApiKey, { usageCount: usedUSD }, session);
-      return transactionResult;
+    const transactionResult = await this.transactionService.create(requestId, {
+      messageId: generateUniqueKey(),
+      collectionId: body.collectionId,
+      apiTokenRef: currentUser.userApiKey,
+      role: 'user',
+      message: body.message,
+      tokenUsage: { usedTokens, usedUSD },
+      llmType: gptModelType,
     });
 
-    return result;
+    await this.apiKeyService.updateUsageCount(currentUser.userApiKey, { usageCount: usedUSD });
+
+    return transactionResult;
   }
 }
