9ba4615ee1f8fad5ca2b60b98bea3bb4a989446a
kingchan818 <chancch70@gmail.com>
2024-07-28 19:25:24 +0800

    FIX: Missing light mode for InputBox in model dropdown component

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -8,13 +8,13 @@ function InputBox({ isActive, onClick }) {
       <label className="relative flex items-center p-3 rounded-full cursor-pointer">
         <input
           type="checkbox"
-          className="before:content[''] peer relative h-8 w-8 cursor-pointer appearance-none rounded-full border border-white/20 bg-white/10 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-white checked:bg-white checked:before:bg-white hover:scale-105 hover:before:opacity-0"
+          className="before:content[''] peer relative h-8 w-8 cursor-pointer appearance-none rounded-full border border-black/20 bg-black/10 dark:border-white/20 dark:bg-white/10 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-gray-300 dark:before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-black checked:bg-black dark:checked:border-white dark:checked:bg-white checked:before:bg-black dark:checked:before:bg-white hover:scale-105 hover:before:opacity-0"
           id="customStyle"
           checked={isActive}
           readOnly
           onClick={onClick}
         />
-        <span className="absolute text-black transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+        <span className="absolute transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100 text-white dark:text-black">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-3.5 w-3.5"
